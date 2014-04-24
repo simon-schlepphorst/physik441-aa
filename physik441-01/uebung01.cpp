9b9f7e1cfd47fa896f6d2fa9b1e2d2ab5945fe4c
Simon Schlepphorst <dev@simon-schlepphorst.de>
2014-04-25 00:09:04 +0200

    Zettel 1 hinzugefügt

---
+++ physik441-01/uebung01.cpp
@@ -0,0 +1,23 @@
+/**
+ * Hallo Welt Program fuer die Numerik Vorlesung 2014
+ * geschrieben von C. Jost
+ *
+ * Das Program gibt den String "Hallo Welt" aus
+ * und erwartet keinen Input.
+ *
+ * g++ uebung01.cpp -o uebung -Wall -Werror
+ * ./uebung
+ **/
+
+// C++ Headerfile, notwendig fuer die Ausgabe
+// von Zeichen auf den Bildschirm
+#include <iostream>
+
+
+int main() {
+
+//  int i;
+  std::cout << "Hallo Welt" << std::endl;
+
+}
+
